72bb67f9a5c5bd5e9bc121ebe782d3766f58cad4
Emils Beloglazovs <emils@clience.co>
2024-03-24 21:21:12 -0400

    Update Logo Click
    Made the logo go to / on click

--- front/components/Shared/Header/LogoTag.tsx
+++ front/components/Shared/Header/LogoTag.tsx
@@ -1,33 +1,43 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { useRouter } from 'next/router'; // Make sure to use 'next/router' instead of 'next/navigation'
 
 const logoContainerStyle = css`
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
+    cursor: pointer; // Make it look clickable
 `;
 
 const logoImageStyle = css`
-  height: 4rem;
-  width: auto;
+    height: 4rem;
+    width: auto;
 `;
 
 const logoTextStyle = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.5rem;
-  font-weight: bold;
-  background-color: var(--primary-color);
-  color: var(--secondary-color);
-  height: 4rem;
-  width: 8rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    font-weight: bold;
+    background-color: var(--primary-color);
+    color: var(--secondary-color);
+    height: 4rem;
+    width: 8rem;
 `;
 
-const LogoTag = () => (
-  <div css={logoContainerStyle}>
-    <img css={logoImageStyle} src="/images/logo-calwc.png" alt="Logo" />
-    <span css={logoTextStyle}>CALWC</span>
-  </div>
-);
+const LogoTag = () => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push('/');
+    };
+
+    return (
+        <div css={logoContainerStyle} onClick={handleClick}>
+            <img css={logoImageStyle} src="/images/logo-calwc.png" alt="Logo" />
+            <span css={logoTextStyle}>CALWC</span>
+        </div>
+    );
+};
 
 export default LogoTag;
