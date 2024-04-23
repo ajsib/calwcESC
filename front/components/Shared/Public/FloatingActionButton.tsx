cde53fcbbe98178c3f6f55ab0181e842a6b5c6e2
lytekm <kevin.morrison653@gmail.com>
2024-04-23 14:03:18 -0400

    changed color of menu button

--- front/components/Shared/Public/FloatingActionButton.tsx
+++ front/components/Shared/Public/FloatingActionButton.tsx
@@ -8,7 +8,7 @@ interface FloatingActionButtonProps {
 const fabStyle = css`
   width: 4rem;
   height: 4rem;
-  background-color: rgba(0, 123, 255, 0.8);
+  background-color: rgba(203, 41, 28, 0.8);
   color: white;
   border-radius: 50%;
   display: flex;
@@ -17,6 +17,9 @@ const fabStyle = css`
   font-size: 1.4rem;
   cursor: pointer;
   padding: 4px;
+  position: fixed;
+  bottom: 6rem;
+  right: 1rem;
 `;
 
 const FloatingActionButton: React.FC<FloatingActionButtonProps> = ({ onClick }) => (
