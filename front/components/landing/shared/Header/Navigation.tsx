7b59ad71c6795455cbba961b1e4119b463924788
lytekm <kevin.morrison653@gmail.com>
2024-05-26 09:44:38 -0400

    change to action buttons and login button

--- front/components/landing/shared/Header/Navigation.tsx
+++ front/components/landing/shared/Header/Navigation.tsx
@@ -2,6 +2,7 @@
 import { css } from '@emotion/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router'; 
+import { useAuth } from '@/globalContexts/authContext'; // Import the useAuth hook
 
 const navigationStyle = css`
   display: flex;
@@ -21,15 +22,29 @@ const linkStyle = css`
   }
 `;
 
+const buttonStyle = css`
+  color: var(--secondary-color);
+  background-color: transparent;
+  border: none;
+  font-weight: bold;
+  font-size: 1.2rem;
+  &:hover {
+    text-decoration: underline !important;
+    cursor: pointer;
+  }
+`;
+
 const Navigation = () => {
   const router = useRouter();
   const { locale, asPath } = router;
+  const { person, logout } = useAuth(); // Get person and logout function from useAuth hook
   const toggleLocale = locale === 'en' ? 'fr' : 'en';
   const languageLabel = locale === 'en' ? 'Français' : 'English';  
   const Home = locale === 'en' ? 'Home' : 'Accueil'
   const About = locale === 'en' ? 'About' : 'Notre mission'
   const Services = locale === 'en' ? 'Services' : 'Nos services'
   const Login = locale === 'en' ? 'Login' : 'Se connecter'
+  const Logout = locale === 'en' ? 'Logout' : 'Se deconnecter'
 
   return (
     <nav css={navigationStyle}>
@@ -37,7 +52,11 @@ const Navigation = () => {
       <Link href="/about" css={linkStyle}>{About}</Link>
       <Link href="/services" css={linkStyle}>{Services}</Link>
       <Link href={asPath} locale={toggleLocale} css={linkStyle}>{languageLabel}</Link>
-      <Link href="/login" css={linkStyle}>{Login}</Link>
+      {person ? (
+        <button css={buttonStyle} onClick={logout}>{Logout}</button>
+      ) : (
+        <Link href="/login" css={linkStyle}>{Login}</Link>
+      )}
     </nav>
   );
 };
