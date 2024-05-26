aec9b6b15feafb3abb5db5825d7bdab43e0db0c6
lytekm <kevin.morrison653@gmail.com>
2024-05-25 20:27:56 -0400

    edit to the hero section

--- front/components/landing/HomePage/Hero/HeroSection.tsx
+++ front/components/landing/HomePage/Hero/HeroSection.tsx
@@ -1,15 +1,35 @@
 /** @jsxImportSource @emotion/react */
 import React, { useEffect, useState } from 'react';
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
+import Image from 'next/image';
 import HeroContent from './HeroContent';
 import useNewsReel, { newsItems } from './NewsReel/NewsReel';
 import CardTemplate from './NewsReel/CardTemplate';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
 
 const HeroSection = () => {
   const [offsetY, setOffsetY] = useState(0);
   const [isMobile, setIsMobile] = useState(false);
   const [currentIndex, setCurrentIndex, progressItem, regressItem] = useNewsReel();
-  
+  const [visibleIndex, setVisibleIndex] = useState(currentIndex); // State to manage the currently visible image
+  const [animation, setAnimation] = useState(fadeIn);
 
   const handleScroll = () => {
     const position = window.scrollY;
@@ -31,14 +51,19 @@ const HeroSection = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (visibleIndex !== currentIndex) {
+      setAnimation(fadeOut);
+      const timeout = setTimeout(() => {
+        setVisibleIndex(currentIndex);
+        setAnimation(fadeIn);
+      }, 1000); // Duration of the fade-out animation
+
+      return () => clearTimeout(timeout);
+    }
+  }, [currentIndex, visibleIndex]);
+
   const contentContainerStyle = css`
-    // width: ${isMobile ? '100vw' : '50vw'};
-    // height: ${isMobile ? '30vh' : '100%'};
-    // padding-top: ${isMobile ? '4rem' : '0rem'};
-    // display: flex;
-    // flex-direction: column;
-    // justify-content: center;
-    // align-items: ${isMobile ? 'flex-end' : 'flex-start'};
     z-index: 1;
   `;
 
@@ -47,22 +72,23 @@ const HeroSection = () => {
     align-items: center;
     height: ${isMobile ? '35vh' : '100vh'};
     transition: height 0.3s;
-    // margin-top: -4rem;
     flex-direction: ${isMobile ? 'column' : 'row'};
+    position: relative;
+    overflow: hidden;
+  `;
+
+  const heroImageContainerStyle = css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
   `;
 
   const heroImageStyle = css`
-  background-image: url(${newsItems[currentIndex].imageUrl});
-  background-size: cover;
-  background-position: center;
-  background-attachment: fixed; 
-  width: 100vw;
-  height: calc(100vh);
-  position: absolute;
-  right: 0;
-  top: 0;
-  transition: background-image 0.6s ease-in-out;
-`;
+    animation: ${animation} 1s ease-in-out;
+  `;
 
   const overlayStyle = css`
     position: absolute;
@@ -71,6 +97,7 @@ const HeroSection = () => {
     width: 100%;
     height: 100%;
     background-image: linear-gradient(rgba(0, 0, 0, 0.5) 10%, rgba(0, 0, 0, 0.2) 100%);
+    z-index: 0;
   `;
 
   const newsFeedStyle = css`
@@ -91,11 +118,19 @@ const HeroSection = () => {
       <div css={contentContainerStyle}>
         <HeroContent isMobile={isMobile} />
       </div>
-      <div css={heroImageStyle} >
+      <div css={heroImageContainerStyle}>
+        <Image 
+          src={newsItems[visibleIndex].imageUrl} 
+          alt="Hero background" 
+          layout="fill" 
+          objectFit="cover" 
+          priority 
+          css={heroImageStyle}
+        />
         <div css={overlayStyle}></div>
-        <div css={newsFeedStyle}>
-          <CardTemplate progressItem={progressItem} regressItem={regressItem} {...newsItems[currentIndex].props} />
-        </div>
+      </div>
+      <div css={newsFeedStyle}>
+        <CardTemplate progressItem={progressItem} regressItem={regressItem} {...newsItems[currentIndex].props} />
       </div>
     </div>
   );
