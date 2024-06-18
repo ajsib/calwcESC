f1108525f97ff38354b4a43e944fea97514b596a
ajsib <ajsibley6@gmail.com>
2024-06-18 16:06:17 -0400

    [#4618] Configure CD Pipeline for CALWC backend

--- back/server.js
+++ back/server.js
@@ -1,14 +1,21 @@
-const express = require('express');
+const express = require('express')
 const path = require('path');
 const app = express();
 const imageRoutes = require('./routes/imageRoutes');
 const errorMiddleware = require('./middlewares/errorMiddleware');
+const healthRoute = require('./routes/healthRoute'); 
+
+const basepath = '/calwc-esc-server';
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
-// Image routes
+// Health Check
+app.use(`${basepath}/api/`, healthRoute)
+
+// routes
 app.use('/api/images/', imageRoutes);
+
 
 // Error handling middleware
 app.use(errorMiddleware);
